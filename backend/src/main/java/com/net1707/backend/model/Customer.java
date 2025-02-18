81aa715bc2fe35a2a0e2b8ce279fe81d797f5c95
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-02-19 01:45:06 +0700

    Create Entity

--- backend/src/main/java/com/net1707/backend/model/Customer.java
+++ backend/src/main/java/com/net1707/backend/model/Customer.java
@@ -1,13 +1,15 @@
 package com.net1707.backend.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
+@Data
 @Table(name = "customers")
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +28,13 @@ public class Customer {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuizResult> quizResults;
 }
---
+++ backend/src/main/java/com/net1707/backend/model/Review.java
@@ -0,0 +1,32 @@
+package com.net1707.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "productId", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(length = 255)
+    private String comment;
+}
