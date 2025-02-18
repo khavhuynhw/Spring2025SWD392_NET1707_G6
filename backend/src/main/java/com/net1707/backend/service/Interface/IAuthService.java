e594b7630323e929cfd39412d1cafad725acbfa7
Trung <trungdqse173523@fpt.edu.vn>
2025-02-19 00:16:11 +0700

    Add validation Register and Add product Controller add validation product

---
+++ backend/src/main/java/com/net1707/backend/service/Interface/IAuthService.java
@@ -0,0 +1,11 @@
+package com.net1707.backend.service.Interface;
+
+import com.net1707.backend.dto.LoginRequestDTO;
+import com.net1707.backend.dto.RegisterRequestDTO;
+import com.net1707.backend.dto.StaffRegisterDTO;
+
+public interface IAuthService {
+    String register(RegisterRequestDTO registerRequestDTO);
+    String registerStaff(StaffRegisterDTO staffRegisterDTO);
+    String login(LoginRequestDTO loginRequestDTO);
+}
